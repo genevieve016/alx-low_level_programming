06aa244db1acad2c907f7dce6bdf8d847acd904b
Genevieve <gennyorons@gmail.com>
2023-02-16 10:36:02 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,9 @@
  * main - C program that prints a line to the standard error
  * Return 1 (Successful)
  */
+
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
