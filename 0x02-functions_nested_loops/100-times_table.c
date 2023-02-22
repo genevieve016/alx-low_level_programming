41f3525e3b3e5c393ac4722a54afedac204509b4
Genevieve <gennyorons@gmail.com>
2023-02-21 18:33:01 -0800

    c-function advanced tasks correction

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,19 +1,20 @@
-#include main.h
+#include "main.h"
 
 /**
  * print_times_table - prints the n times table, starting with 0
  *
  * @n: where 0 < n <= 15
+ * Return: Always 0
  */
 
 int print_times_table(void)
 {
-		print_times_table(3);
-		_putchar('\n');
-		print_times_table(5);
-                _putchar('\n');
-                print_times_table(98);
-                _putchar('\n');
-                print_times_table(12);
-                return (0);
+print_times_table(3);
+_putchar('\n');
+print_times_table(5);
+_putchar('\n');
+print_times_table(98);
+_putchar('\n');
+print_times_table(12);
+return (0);
 }
