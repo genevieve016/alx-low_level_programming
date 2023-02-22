b031f089734a2c8bf654fc4ecd8f79129dc7bab4
Genevieve <gennyorons@gmail.com>
2023-02-21 18:26:32 -0800

    c-functions advanced task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,46 +1,19 @@
-include "main.h"
+#include main.h
 
 /**
- * print_times_table - function that prints the n times table,
- * starting with 0
- * @n: number times table where 0 < n <= 15
+ * print_times_table - prints the n times table, starting with 0
  *
- * Return: no return
+ * @n: where 0 < n <= 15
  */
-void print_times_table(int n)
-{
-	int a, b, op;
 
-	if (n >= 0 && n <= 15)
-	{
-		for (a = 0; a <= n; a++)
-		{
-			_putchar(48);
-			for (b = 1; b <= n; b++)
-			{
-				op = a * b;
-				_putchar(44);
-				_putchar(32);
-				if (op <= 9)
-				{
-					_putchar(32);
-					_putchar(32);
-					_putchar(op + 48);
-				}
-				else if (op <= 99)
-				{
-					_putchar(32);
-					_putchar((op / 10) + 48);
-					_putchar((op % 10) + 48);
-				}
-				else
-				{
-					_putchar(((op / 100) % 10) + 48);
-					_putchar(((op / 10) % 10) + 48);
-					_putchar((op % 10) + 48);
-				}
-			}
-			_putchar('\n');
-		}
-	}
+int print_times_table(void)
+{
+		print_times_table(3);
+		_putchar('\n');
+		print_times_table(5);
+                _putchar('\n');
+                print_times_table(98);
+                _putchar('\n');
+                print_times_table(12);
+                return (0);
 }
