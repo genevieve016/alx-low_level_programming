3d481023f49902db8fb88be343f0d4aad928c130
Genevieve <gennyorons@gmail.com>
2023-02-23 15:17:56 -0800

    dxbug

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,24 +10,20 @@
 
 int largest_number(int a, int b, int c)
 {
-int largest;
+ int largest;
 
-if (a > b && a > c)
+ if (a > b && a > c)
+ {
+ largest = a;
+ }
+ else if (a > b && c > a)
+ {
+ largest = c;
+ }
+ else
 {
-largest = a;
-}
-else if (a > b && c > a)
-{
-largest = c;
-}
-else if (b > c)
-{
-largest = b;
-}
-else
-{
-largest = c;
-}
+ largest = b;
+ }
 
 return (largest);
 }
