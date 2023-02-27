3d3b621d7451c31d0b4b54ecaf930ce81ec23c1e
Genevieve <gennyorons@gmail.com>
2023-02-27 01:34:20 -0800

    0x05. C - Pointers, arrays and strings Tasks

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,7 +2,7 @@
 
 /**
  * char *_strcpy - function that copies the string pointed to by src
- * including the terminating null byte (\0), to the buffer pointed to by dest.
+ * including the terminating null byte (\0), to the buffer pointed to by dest
  * @dest: copy to
  * @src: copy from
  * Return: string
