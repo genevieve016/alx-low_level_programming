37e5b47f1570c8098dadf3c40c55c3140f13b167
Genevieve <gennyorons@gmail.com>
2023-03-06 17:40:32 -0800

    7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_chessboard - function that prints the chessboard
- * @a: array for chessboard
+ * @a: array
  * Return: Always 0
  */
 void print_chessboard(char (*a)[8])
