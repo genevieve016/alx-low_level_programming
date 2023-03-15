97d453325d21742f57897d1a4a03a6d02b09cf65
Genevieve <gennyorons@gmail.com>
2023-03-14 22:53:37 -0700

    0x0B. C - malloc, free Correction

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,52 +3,34 @@
 
 /**
  * str_concat - function that concatenates two strings
- * @s1: input one to concat
- * @s2: input two to concat
- * Return: concat of s1 and s2
+ * @width: input one to concat
+ * @height: input two to concat
+ * Return: concat of width and height
  */
 
-char *str_concat(char *s1, char *s2)
-
+int **alloc_grid(int width, int height)
 {
-	char *conct;
-	int i, ci;
-
-	if (s1 == NULL)
-		s1 = "";
-
-	if (s2 == NULL)
-		s2 = "";
-
-		i = ci = 0;
-
-	while (s1[i] != '\0')
-		i++;
-
-	while (s2[ci] != '\0')
-		ci++;
-
-	conct = malloc(sizeof(char) * (i + ci + 1));
-
-
-	if (conct == NULL)
+	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	i = ci = 0;
-	while (s1[i] != '\0')
 
+	int **grid = malloc(sizeof(int *) * height);
+
+	if (grid == NULL)
+		return (NULL);
+
+
+	for (int i = 0; i < height; i++)
 	{
-		conct[i] = s1[i];
-		i++;
+		grid[i] = calloc(width, sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (int j = 0; j < i; j++)
+				free(grid[j]);
+			free(grid);
+			return (NULL);
+		}
 	}
 
-	while (s2[ci] != '\0')
-
-	{
-		conct[i] = s2[ci];
-		i++, ci++;
-	}
-
-	conct[i] = '\0';
-	return (conct);
+	return (grid);
 }
