817a2ac46cdcdc08aca8ed26e14b8d9256e6798c
Genevieve <gennyorons@gmail.com>
2023-03-16 22:07:48 -0700

    0x0C. C - More malloc, free All Tasks

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -38,7 +38,7 @@ int _strlen(char *s)
 
 {
 	int i = 0;
-	
+
 	while (s[i] != '\0')
 	{
 		i++;
@@ -71,64 +71,43 @@ int main(int argc, char *argv[])
 
 {
 	char *s1, *s2;
-	
+
 	int s1_len, s2_len, len, i, carry, digit1, digit2, *s, a = 0;
-	
+
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
 	s1_len = _strlen(s1);
 	s2_len = _strlen(s2);
 	len = s1_len + s2_len + 1;
-	
 	s = malloc(sizeof(int) * len);
 	if (!s)
 		return (1);
 	for (i = 0; i <= s1_len + s2_len; i++)
 		s[i] = 0;
 	for (s1_len = s1_len - 1; s1_len >= 0; s1_len--)
-	{
-		digit1 = s1[s1_len] - '0';
+	{	digit1 = s1[s1_len] - '0';
 		carry = 0;
 		for (s2_len = _strlen(s2) - 1; s2_len >= 0; s2_len--)
 		{
 			digit2 = s2[s2_len] - '0';
-
-                        carry += s[s1_len + s2_len  + 1] + (digit1 * digit2);
-                        s[s1_len + s2_len + 1] = carry % 10;
-
-                        carry /= 10;
-
-                }
-
-                if (carry > 0)
-
-                        result[len1 + len2 + 1] += carry;
-
-        }
-
-        for (i = 0; i < len - 1; i++)
-
-        {
-
-                if (result[i])
-
-                        a = 1;
-
-                if (a)
-
-                        _putchar(result[i] + '0');
-
-        }
-
-        if (!a)
-
-                _putchar('0');
-
-        _putchar('\n');
-
-        free(result);
-
-        return (0);
-
+			carry += s[s1_len + s2_len  + 1] + (digit1 * digit2);
+			s[s1_len + s2_len + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			s[s1_len + s2_len + 1] += carry;
+	}
+	for (i = 0; i < len - 1; i++)
+	{
+		if (s[i])
+			a = 1;
+		if (a)
+			_putchar(s[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
+	return (0);
 }
